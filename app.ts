9944a2ec0e71dc2ebd60db79d669ddd4ec1146d7
Elliot <ebu0729@student.skelleftea.se>
2025-12-17 09:39:25 +0100

    Fortsatt arbete 5

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 
 
 let amount_jumps = 2
-
+let amount_dashes = 2
 let char_x = 50
 let char_y = 450
 let movement_x = 0
@@ -11,37 +11,51 @@ let jump_time = 0
 let character = new Hitbox (char_x,char_y, 25, 50)
 let dash_time = 0
 let dashing = false
+let after_dash = false
+
 
 function jump () {
-    if (dashing && keyboard.d || keyboard.a && dashing) {
+    if (dashing && (keyboard.d || keyboard.a) && amount_dashes > 0 ) {
+        
         return 0
-    }
-    else if(jumping && char_y <= 450) {
-    if (!dashing) {jump_time +=deltaTime}
+    } else if(after_dash && char_y < 451) {
+        jump_time = 350
+        after_dash = false
+        
+    } 
+    else if(jumping && char_y <= 450 && after_dash == false) {
+        
+        jump_time += deltaTime
    return -10 + 10 * jump_time/350
     
-} else if (char_y > 450) {
+} else if (char_y = 450) {
     jumping = false
+    dashing = false
     char_y = 450
     jump_time = 0
     amount_jumps = 2
-    if (dashing) {return 0}
-    else if (!dashing) {return 0}
+    amount_dashes = 2
+    return 0
 }
 return 0
 }
 
 function dash () {
     
-    if (dashing && keyboard.d && dash_time < 200) {
+    if (dashing && keyboard.d && dash_time < 200 && amount_dashes>0) {
         dash_time += deltaTime
+        
         return 10 
-    } else if (dashing && keyboard.a && dash_time < 200) {
+    } else if (dashing && keyboard.a && dash_time < 200 && amount_dashes>0) {
         dash_time += deltaTime
+        
         return -10
-    }else if (dash_time >= 200) {
+    }else if (dashing && dash_time >= 200) {
+        amount_dashes--
         dash_time = 0
         dashing = false
+        after_dash = true
+        
     }
     return 0
 }
@@ -49,7 +63,7 @@ function dash () {
 
 
 function walk () {
-    if (dashing ) {
+    if (dashing && (keyboard.a || keyboard.d) && amount_dashes > 0) {
         return movement_x
     }
     else if (keyboard.a) {
@@ -89,10 +103,14 @@ update = () => {
     ground.drawOutline ()
     movement_x = walk() //+ dash()
     movement_y = jump() 
-    if (keyboard.shift) {
+    if (keyboard.shift && (keyboard.a || keyboard.d)) {
         keyboard.shift = false
         dashing = true 
-     }
+        
+        if(amount_dashes== 1 && dashing){
+            dash_time = 0
+        }
+    }
     
     
     
