1ec011fdf3251dc558ef0f63f482c9569d527306
Elliot <ebu0729@student.skelleftea.se>
2026-01-14 09:48:20 +0100

    Fortsatt arbete 7

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@
 let amount_jumps = 2
 let amount_dashes = 2
 let char_x = 50
-let char_y = 450
+let char_y = 400
 let movement_x = 0
 let movement_y = 0
 let jumping = false
@@ -12,26 +12,68 @@ let character = new Hitbox (char_x,char_y, 25, 50)
 let dash_time = 0
 let dashing = false
 let after_dash = false
+let lager_3 = []
+let lager_4 = [] 
 
 
+let grassblock1 = await fetchImage("images/Block1.png")
+let stoneblock1 = await fetchImage("images/stoneblock1.png")
+let Dirtoverlayblock = await fetchImage("images/Dirtoverlayblock.png")
+let Dirtblock = await fetchImage("Images/Dirtblock.png")
+let Dirtblockhitbox = new Hitbox(Dirtblock[0], Dirtblock[1], Dirtblock[2], Dirtblock[3])
+let hitboxdirtoverlayblock = new Hitbox (Dirtoverlayblock[0], Dirtoverlayblock[1], Dirtoverlayblock[2], Dirtoverlayblock[3])
+let hitboxgrassblock1 = new Hitbox(grassblock1[0], grassblock1[1]-200, 25, 25)
+let hitboxstoneblock1 = new Hitbox(stoneblock1[0], stoneblock1[1], stoneblock1[2], stoneblock1[3])
+
+
+
+
+for(let i = 0; i<= 20; i++) {
+    lager_3.push (random(1, 5))
+    lager_4.push (random(1, 5))
+}
+function draw_map () {
+for (let i = 0; i<=20; i++) {
+ctx.drawImage(grassblock1, i*25, 450, 25,25)
+ctx.drawImage(stoneblock1, i*25, 550, 25, 25)
+ctx.drawImage(Dirtblock, i*25, 500, 25, 25)
+ctx.drawImage(Dirtblock, i*25, 475, 25, 25)
+ctx.drawImage(stoneblock1,i*25,550,25,25)
+ctx.drawImage(stoneblock1,i*25,575,25,25)
+ctx.drawImage(stoneblock1,i*25,600,25,25)
+
+if (lager_3[i] == 1 || lager_3[i] > 2) {ctx.drawImage(Dirtblock,i*25,500,25,25)}
+
+else if(lager_3[i] == 2 ) {ctx.drawImage(stoneblock1,i*25,500,25,25)}
+
+if(lager_4[i]==2) {ctx.drawImage(Dirtblock,i*25,525,25,25)}
+else if(lager_4[i]==1 || lager_4[i]>2) {ctx.drawImage(stoneblock1,i*25,525,25,25)}
+
+}
+
+
+
+}
+let hitboxii = new Hitbox (0, 450, 25*lager_3.length, 125 )
 function jump () {
     if (dashing && (keyboard.d || keyboard.a) && amount_dashes > 0 ) {
         
         return 0
-    } else if(after_dash && char_y < 451) {
+    } else if(after_dash && char_y < 401) {
         jump_time = 350
         after_dash = false
         
     } 
-    else if(jumping && char_y <= 450 && after_dash == false) {
+    else if(jumping && char_y <= 400 && after_dash == false) {
         
         jump_time += deltaTime
    return -10 + 10 * jump_time/350
     
-} else if (char_y = 450) {
+} else if (character.intersects (hitboxii)) {
+    console.log("intersect")
     jumping = false
     dashing = false
-    char_y = 450
+    char_y = hitboxii[1] -50
     jump_time = 0
     amount_jumps = 2
     amount_dashes = 2
@@ -46,7 +88,7 @@ function dash () {
     //console.log (amount_dashes)
     if ( keyboard.d && dash_time < 200 && amount_dashes>0) {
         //dash_time += deltaTime
-        console.log (dash_time)
+        //console.log (dash_time)
         return 10 
     } else if (keyboard.a && dash_time < 200 && amount_dashes>0) {
         //dash_time += deltaTime
@@ -92,6 +134,7 @@ rectangle(x, y, 25, 50, "red")
 hitbox.x = x
 hitbox.y = y
 hitbox.drawOutline()
+
 }
 
 
@@ -101,10 +144,12 @@ let ground = new Hitbox(0,500,5000,5000)
 
 update = () => {
     clear()
-   
-    rectangle(300, 450,50,50, "red")
-    rectangle(0,500, 5000, 5000, "green")
-    ground.drawOutline ()
+    
+    draw_map()
+    
+    //rectangle(300, 450,50,50, "red")
+    //rectangle(0,500, 5000, 5000, "green")
+    //ground.drawOutline ()
     movement_x = walk() //+ dash()
     movement_y = jump() 
     if (keyboard.shift && (keyboard.a || keyboard.d)) {
@@ -135,7 +180,12 @@ update = () => {
     updateCharacter(char_x, char_y, character)
       
 } 
- let grassblock1 = await fetchImage("images/Block1.png")
+ 
+
+
+export { }
+/*
+let grassblock1 = await fetchImage("images/Block1.png")
 let stoneblock1 = await fetchImage("images/stoneblock1.png")
 let Dirtoverlayblock = await fetchImage("images/Dirtoverlayblock.png")
 let Dirtblock = await fetchImage("Images/Dirtblock.png")
@@ -164,4 +214,4 @@ else if(m==1 || m>2) {ctx.drawImage(stoneblock1,i,525,25,25)}
 }
 }
 
-
+*/
